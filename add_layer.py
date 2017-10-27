317f3e5c37c95ea2256ccf665cdda0e5e7dc23f0
froglake <froglake@froglake.cs.ualberta.ca>
2017-10-27 17:53:04 -0600

    first commit

---
+++ add_layer.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import numpy as np
+
+def add_layer(inputs,in_size,out_size,activation_fun=None):
+    Weights = tf.Variable(tf.random_normal([in_size,out_size]))
+    biases = tf.Variable(tf.zeros([1,out_size])+.1)
+    Wx_plus_b = tf.matmul(inputs,Weights)+biases
+    if activation_fun is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_fun(Wx_plus_b)
+    return outputs
+
+xs = tf.placeholder(tf.float32,[None,1])
+ys = tf.placeholder(tf.float32,[None,1])
+
+x_data = np.linspace(-1,1,300,dtype=np.float32)[:,np.newaxis]
+noise = np.random.normal(0,0.05,x_data.shape).astype(np.float32)
+y_data = np.square(x_data) - .5 + noise
+
+l1 = add_layer(x_data,1,10,activation_fun =tf.nn.relu)
+prediction = add_layer(l1,10,1, activation_fun = None)
+
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
+                     reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+#init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
+
+sess = tf.Session()
+
+sess.run(init)
+
+
+for i in range(1000):
+    sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
+    if i%50 == 0:
+         print (sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
+
+
+
+
+
