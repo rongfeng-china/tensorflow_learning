7084f30cb2fbeadb758195d1339b619f69fa4565
froglake <froglake@froglake.cs.ualberta.ca>
2017-10-27 18:01:12 -0600

    update2

--- classification.py
+++ classification.py
@@ -2,15 +2,15 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 
-def add_layer(inputs, in_size, out_size, activation_function=None,):
+def add_layer(inputs, in_size, out_size, activation_fun=None,):
     # add one more layer and return the output of this layer
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
     biases = tf.Variable(tf.zeros([1, out_size]) + 0.1,)
     Wx_plus_b = tf.matmul(inputs, Weights) + biases
-    if activation_function is None:
+    if activation_fun is None:
         outputs = Wx_plus_b
     else:
-        outputs = activation_function(Wx_plus_b,)
+        outputs = activation_fun(Wx_plus_b,)
     return outputs
 
 
@@ -32,7 +32,7 @@ y_data = np.square(x_data) - .5 + noise'''
 
 prediction = add_layer(xs,784,10, activation_fun = tf.nn.softmax)
 
-cross_entropy = tf.reduce_mean(tf.reduce_sum(-tf.reduce_sum(ys*tf.log(prediction),reduction_indices=[1])))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys*tf.log(prediction),reduction_indices=[1]))
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
